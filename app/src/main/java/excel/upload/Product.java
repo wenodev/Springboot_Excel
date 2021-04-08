c0ee2c8c12014452d2fcaaeb9e0816faa5d731d6
weno <logicalstark1105@gmail.com>
2021-04-08 16:17:36 +0900

    feat: Product 변수 생성하라

--- app/src/main/java/excel/upload/Product.java
+++ app/src/main/java/excel/upload/Product.java
@@ -1,13 +1,45 @@
 package excel.upload;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@NoArgsConstructor
+@Getter
 @Entity
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private Integer price;
+
+    @Column
+    private Double weight;
+
+    @Column
+    private String description;
+
+    @Column
+    private String imgUrl;
+
+    @Builder
+    public Product(Long id, String name, Integer price, Double weight, String description, String imgUrl) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.weight = weight;
+        this.description = description;
+        this.imgUrl = imgUrl;
+    }
 }
