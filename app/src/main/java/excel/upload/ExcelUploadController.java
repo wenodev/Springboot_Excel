5738822df164522da841a42fb4c652549c529e6a
weno <logicalstark1105@gmail.com>
2021-04-08 14:49:16 +0900

    feat: uploadProduct 생성하라

--- app/src/main/java/excel/upload/ExcelUploadController.java
+++ app/src/main/java/excel/upload/ExcelUploadController.java
@@ -1,7 +1,18 @@
 package excel.upload;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/upload/product")
 @RestController
 public class ExcelUploadController {
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public void uploadProduct(){
+
+    }
 }
