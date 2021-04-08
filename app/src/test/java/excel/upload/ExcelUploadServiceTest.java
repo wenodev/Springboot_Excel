8fb8f0c43d5183ce387878e03470bf5d62f90618
weno <logicalstark1105@gmail.com>
2021-04-08 15:45:38 +0900

    test: 엑셀 데이터 불러오기 성공하라

--- app/src/test/java/excel/upload/ExcelUploadServiceTest.java
+++ app/src/test/java/excel/upload/ExcelUploadServiceTest.java
@@ -1,5 +1,10 @@
 package excel.upload;
 
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.core.io.ClassPathResource;
@@ -20,6 +25,14 @@ class ExcelUploadServiceTest {
     @Test
     void testGetFile() throws IOException {
         assertThat(file.getFilename()).isEqualTo("spring-excel-upload-product.xlsx");
+    }
+
+    @Test
+    void getWorkBookData() throws IOException {
+        Workbook workbook = new XSSFWorkbook(file.getInputStream());
+        Sheet workSheet = workbook.getSheetAt(0);
+        Row firstRow = workSheet.getRow(0);
+        assertThat(firstRow.getCell(0).getStringCellValue()).isEqualTo("상품명");
     }
 
 }
