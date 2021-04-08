abafcdc0ec3b3a849fd9c6d0ace27829f03f4d3f
weno <logicalstark1105@gmail.com>
2021-04-08 16:14:51 +0900

    feat: Product Entity 생성하라

---
+++ app/src/main/java/excel/upload/Product.java
@@ -0,0 +1,13 @@
+package excel.upload;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
