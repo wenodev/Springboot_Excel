d80599738696c036dddbcee18840d71e7efc2c3a
weno <logicalstark1105@gmail.com>
2021-04-09 17:54:50 +0900

    feat: router 추가하라

--- app-api/src/main/java/excel/upload/controller/ProductController.java
+++ app-api/src/main/java/excel/upload/controller/ProductController.java
@@ -20,4 +20,7 @@ public class ProductController {
     public List<Product> list(){
         return productService.list();
     }
+
+    
+
 }
--- app-web/src/App.js
+++ app-web/src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import UploadFrom from "./components/UploadForm";
 import ProductList from "./components/ProductList";
+import { Route } from "react-router-dom";
+import ProductDetail from "./components/ProductDetail";
 
 function App() {
   return (
     <div className="App">
-      <UploadFrom />
-      <ProductList />
+      <Route exact component={ProductList} path="/" />
+      <Route exact component={ProductList} path="/products" />
+      <Route exact component={ProductDetail} path="/products/:id" />
+      <Route component={UploadFrom} path="/upload" />
+      {/* <ProductList /> */}
     </div>
   );
 }
