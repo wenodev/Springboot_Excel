5f0f492e44f553f26e828c18f7b13f82c5ab8bce
weno <logicalstark1105@gmail.com>
2021-04-09 16:00:45 +0900

    fix: controller 버그 수정

--- app-api/src/test/java/excel/upload/controller/ProductControllerTest.java
+++ app-api/src/test/java/excel/upload/controller/ProductControllerTest.java
@@ -3,12 +3,13 @@ package excel.upload.controller;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WebMvcTest(ProductController.class)
 class ProductControllerTest {
 
     @Autowired
