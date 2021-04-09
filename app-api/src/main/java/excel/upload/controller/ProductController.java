71816625ecf90c575d6b713934c46fc33b4063da
weno <logicalstark1105@gmail.com>
2021-04-09 16:01:37 +0900

    feat: ProductService 생성하라

--- app-api/src/main/java/excel/upload/controller/ProductController.java
+++ app-api/src/main/java/excel/upload/controller/ProductController.java
@@ -1,6 +1,7 @@
 package excel.upload.controller;
 
 import excel.upload.entity.Product;
+import excel.upload.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,13 @@ import java.util.List;
 
 @RestController
 public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
     @GetMapping("/products")
     public List<Product> list(){
         return null;
---
+++ app-api/src/main/java/excel/upload/service/ProductService.java
@@ -0,0 +1,10 @@
+package excel.upload.service;
+
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Service
+public class ProductService {
+}
