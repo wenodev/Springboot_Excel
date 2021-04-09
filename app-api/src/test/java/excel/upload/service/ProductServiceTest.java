96d48f8718f769ee12ca6b878688d855522a93c2
weno <logicalstark1105@gmail.com>
2021-04-09 16:08:26 +0900

    test: list 실패하라

--- app-api/src/test/java/excel/upload/service/ProductServiceTest.java
+++ app-api/src/test/java/excel/upload/service/ProductServiceTest.java
@@ -1,9 +1,14 @@
 package excel.upload.service;
 
+import excel.upload.entity.Product;
 import excel.upload.entity.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
 class ProductServiceTest {
@@ -16,5 +21,21 @@ class ProductServiceTest {
         productRepository = mock(ProductRepository.class);
         productService = new ProductService(productRepository);
     }
-    
+
+    @Test
+    void testList(){
+
+        Product product = Product.builder()
+                .id(1L)
+                .name("dummy-name-existed")
+                .price(2500)
+                .weight(3.1)
+                .description("dummy-name-description")
+                .imgUrl("dummy-name-imgUrl")
+                .build();
+
+        given(productRepository.findAll()).willReturn(List.of(product));
+        List<Product> products = productService.list();
+    }
+
 }
