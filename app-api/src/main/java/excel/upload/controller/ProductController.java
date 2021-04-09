32889de0da3077ce7e5a4f65e6b77529676b7118
weno <logicalstark1105@gmail.com>
2021-04-09 15:59:44 +0900

    feat: list 메소드 생성하라

--- app-api/src/main/java/excel/upload/controller/ProductController.java
+++ app-api/src/main/java/excel/upload/controller/ProductController.java
@@ -1,8 +1,16 @@
 package excel.upload.controller;
 
+import excel.upload.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class ProductController {
+    @GetMapping("/products")
+    public List<Product> list(){
+        return null;
+    }
 }
