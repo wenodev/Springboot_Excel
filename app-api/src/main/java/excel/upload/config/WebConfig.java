993beb2be42f411df26cae6da05a5e42450802a8
weno <logicalstark1105@gmail.com>
2021-04-09 15:41:05 +0900

    fix: cors를 해결하기위해 WebConfig 설정하라

--- app-api/src/main/java/excel/upload/config/WebConfig.java
+++ app-api/src/main/java/excel/upload/config/WebConfig.java
@@ -1,2 +1,15 @@
-package excel.upload.config;public class WebConfig {
+package excel.upload.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000")
+                .allowedMethods("GET", "POST", "PUT", "DELETE");
+    }
 }
